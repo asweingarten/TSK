bd2c803204240d51c0f5cd06f07e29b22e6c2d2a
ARIEL WEINGARTEN <ASWEINGA@uwaterloo.ca>
2014-06-02 22:11:35 -0400

    Removed commented out bits.

--- typing game/model/TypingGameModel.java
+++ typing game/model/TypingGameModel.java
@@ -4,12 +4,7 @@ import java.util.ArrayList;
 
 public class TypingGameModel
 {
-	// TypingLevel currentLevel_;
-	// Array<TypingLevel> pastLevels_;
-
-	public TypingGameModel()
-	{
-		// currentLevel_ = new TypingLevel();
-		// pastLevels_ = new ArrayList<TypingLevel>();
-	}
+    public TypingGameModel()
+    {
+    }
 }
