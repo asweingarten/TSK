cc77aadc2b1941ddad18496c7632549d4ef74cb3
ARIEL WEINGARTEN <ASWEINGA@uwaterloo.ca>
2014-06-07 11:35:45 -0400

    Shortened the name of "TypingLevelXXX" classes to "LevelXXX" to avoid rendundantly lengthy names.

---
+++ tsk typer/presenter/LevelPresenter.java
@@ -0,0 +1,12 @@
+package presenter;
+import model.*;
+
+public class LevelPresenter
+{
+    TypingLevelModel model_;
+
+    public LevelPresenter()
+    {
+        this.model_ = new TypingLevelModel();
+    }
+}
---
+++ tsk typer/view/LevelComponent.java
@@ -0,0 +1,118 @@
+package view;
+import presenter.*;
+
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+
+public class LevelComponent extends JComponent implements IView
+{
+  private EventListenerList listenerList_ = new EventListenerList();
+
+
+  private final int width_,
+                    height_;
+
+  private char lastTypedChar;
+
+  private TypingLevelPresenter presenter_;
+
+  // Components
+  private JLabel previouslyTyped,
+                 beingTyped,
+                 toBeTyped;
+
+  public LevelComponent( int width, int height )
+  {
+    super();
+    width_  = width;
+    height_ = height;
+    this.setLayout( new FlowLayout( FlowLayout.LEFT, width_/5, height_/3 ) );
+
+    // Text to be typed
+    previouslyTyped = new JLabel( "Previously Typed", JLabel.LEFT );
+    beingTyped = new JLabel( "Being Typed", JLabel.CENTER );
+    toBeTyped = new JLabel( "To Be Typed", JLabel.RIGHT );
+
+    this.add( previouslyTyped );
+    this.add( beingTyped );
+    this.add( toBeTyped );
+
+    addMouseListener( new MouseAdapter()
+    {
+        public void mousePressed( MouseEvent e )
+        {
+          fireActionPerformed( new ActionEvent( this, 0, "MOUSE_PRESSED" ) );
+          System.out.println( "Mouse Pressed" );
+        }
+
+        public void mouseReleased( MouseEvent e )
+        {
+        }
+    });
+
+    addKeyListener( new KeyAdapter()
+    {
+      public void keyTyped( KeyEvent e )
+      {
+        fireActionPerformed( new ActionEvent( this, 0, "KEY_TYPED" ) );
+        System.out.println( "A key was typed: " + e.getKeyChar() );
+        lastTypedChar = e.getKeyChar();
+        saveToPresenter();
+      }
+
+      public void keyReleased( KeyEvent e )
+      {
+        updateFromPresenter();
+      }
+
+    });
+
+  }
+
+  public void paintComponent( Graphics g )
+  {
+    Graphics2D g2 = ( Graphics2D ) g;
+    g2.setPaint( Color.WHITE );
+    g2.fillRect( 0, 0, width_, height_ );
+
+    g2.setPaint( Color.BLACK );
+    // previouslyTyped.paint( g2 );
+  }
+
+  public void updateFromPresenter()
+  {
+
+  }
+
+  public void saveToPresenter()
+  {
+    // presenter_.setLastEnteredKey();
+  }
+
+  //***************************
+  //*   Event Listener code
+  //***************************
+  public void addActionListener( ActionListener l )
+  {
+      listenerList_.add( ActionListener.class, l );
+  }
+
+  public void removeActionListener( ActionListener l )
+  {
+      listenerList_.remove( ActionListener.class, l );
+  }
+
+  protected void fireActionPerformed( ActionEvent e )
+  {
+      Object[] listeners = listenerList_.getListenerList();
+      for (int i = listeners.length-2; i>=0; i-=2)
+      {
+          if (listeners[i]==ActionListener.class)
+              ((ActionListener)listeners[i+1]).actionPerformed(e);
+      }
+
+  }
+}
