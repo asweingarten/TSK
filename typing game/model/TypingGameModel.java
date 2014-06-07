94b2f5f9ae2a989fddbe9efd599e84854558a76f
saba <saba@saba-RF511-RF411-RF711>
2014-06-07 00:58:44 -0400

    Added file reading code to model.

--- typing game/model/TypingGameModel.java
+++ typing game/model/TypingGameModel.java
@@ -4,7 +4,15 @@ import java.util.ArrayList;
 
 public class TypingGameModel
 {
+    private TextFileReader reader_;
+
     public TypingGameModel()
     {
+        reader_ = new TextFileReader(new FullFileReadStrategy());
+    }
+
+    public String GetFileContents(String filename)
+    {
+        return reader_.ReadFile(filename);
     }
 }
