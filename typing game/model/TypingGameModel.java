fd7c5522eb3d6979f7f3f7b757240a408d039ada
saba <saba@saba-RF511-RF411-RF711>
2014-06-07 01:24:40 -0400

    Added and implemented SetupScreenPresenter.java

--- typing game/model/TypingGameModel.java
+++ typing game/model/TypingGameModel.java
@@ -1,18 +1,50 @@
 package model;
 
 import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
 
 public class TypingGameModel
 {
     private TextFileReader reader_;
+    private String filename_;
 
     public TypingGameModel()
     {
         reader_ = new TextFileReader(new FullFileReadStrategy());
     }
 
-    public String GetFileContents(String filename)
+    public String GetFilename()
     {
-        return reader_.ReadFile(filename);
+        return filename_;
+    }
+
+    public void SetFilename(String filename)
+    {
+        filename_ = filename;
+    }
+
+    public String GetFileContents()
+    {
+        try
+        {
+            return reader_.ReadFile(filename_);
+        }
+        catch (IOException ex)
+        {
+            return "";
+        }
+    }
+
+    public boolean IsFileValid()
+    {
+        try
+        {
+            return new File(filename_).isFile();
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
     }
 }
--- typing game/presenter/TypingGamePresenter.java
+++ typing game/presenter/TypingGamePresenter.java
@@ -9,4 +9,24 @@ public class TypingGamePresenter
     {
         this.model_ = new TypingGameModel();
     }
+
+    public String GetFilename()
+    {
+        return model_.GetFilename();
+    }
+
+    public void SetFilename(String filename)
+    {
+        model_.SetFilename(filename);
+    }
+
+    public String GetFileContents()
+    {
+        return model_.GetFileContents();
+    }
+
+    public boolean IsGameReady()
+    {
+        return model_.IsFileValid();
+    }
 }
