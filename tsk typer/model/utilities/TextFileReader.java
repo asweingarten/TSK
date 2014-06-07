05cd0592c3567afceea9a01df3aa6a393da5cc13
ARIEL WEINGARTEN <ASWEINGA@uwaterloo.ca>
2014-06-07 12:56:22 -0400

    Moved file reading code into "utilities" subfolder in the models folder. They repesent a tool used by models of the game.

---
+++ tsk typer/model/utilities/TextFileReader.java
@@ -0,0 +1,18 @@
+package model.utilities;
+
+import java.io.IOException;
+
+public class TextFileReader
+{
+    private IReadStrategy strategy_;
+
+    public TextFileReader(IReadStrategy strategy)
+    {
+        this.strategy_ = strategy;
+    }
+
+    public String ReadFile(String filename) throws IOException
+    {
+        return this.strategy_.ReadFile(filename);
+    }
+}
