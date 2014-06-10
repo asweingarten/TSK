ca5d012f468d61098672bee13517ebd81baf0cc8
Mark Roukema <mroukema@gmail.com>
2014-06-10 15:25:23 -0400

    Added logic to echo program to get and iterate through list of comm ports and find a serial port with a name match the targetSerialPort Variable

--- tsk typer/view/utilities/TouchKeyEcho.java
+++ tsk typer/view/utilities/TouchKeyEcho.java
@@ -1,14 +1,52 @@
 import java.io.IOException;
+import java.util.Enumeration;
 import javax.comm.SerialPort;
+import javax.comm.CommPortIdentifier;
+import javax.comm.PortInUseException;
 
 public class TouchKeyEcho
 {
+    private final String targetPortName = "COM2";
+    private SerialPort serialPort = null;
+
+    public static void main(String[] args)
+    {
+        TouchKeyEcho touchKeyEcho = new TouchKeyEcho();
+    }
 
     public TouchKeyEcho()
     {
         //Find serial port
-        //Create TouchKeyEchoHandler
-        //Add TouchKeyEchoHandler as event lister to serial port
+        Enumeration portList = CommPortIdentifier.getPortIdentifiers();
+        while(portList.hasMoreElements())
+        {
+            CommPortIdentifier port = (CommPortIdentifier)portList.nextElement();
+            if(port.getPortType() == CommPortIdentifier.PORT_SERIAL) 
+            {
+                if(port.getName().equals(targetPortName))
+                {
+                    try
+                    {
+                        serialPort = (SerialPort)port.open(this.toString(),500);
+                    }
+                    catch(PortInUseException e)
+                    {
+                        System.err.println(e);
+                    }
+                }
+            }
+        }
+        try
+        {
+            //Create TouchKeyEchoHandler
+            TouchKeyEchoHandler touchKeyEchoHandler = new TouchKeyEchoHandler(serialPort);
+            //Add TouchKeyEchoHandler as event lister to serial port
+            serialPort.addEventListener(touchKeyEchoHandler);
+        }
+        catch(Exception e)
+        {
+            System.err.println(e);
+        }
     }
 
     public class TouchKeyEchoHandler extends TouchKeyHandler
