315c8b61656730293701d5ae04e624403b2fa8f7
Mark Roukema <mroukema@gmail.com>
2014-06-12 12:46:29 -0400

    Pushing lateset for Ariel

--- tsk typer/view/utilities/SerialPortPrinter.java
+++ tsk typer/view/utilities/SerialPortPrinter.java
@@ -1,15 +1,29 @@
 import java.io.IOException;
 import java.util.Enumeration;
+import jssc.SerialPort;
+import jssc.SerialPortException;
 import jssc.SerialPortList;
 public class SerialPortPrinter 
 {
     public static void main(String[] args)
     {
+     //   System.out.println(SerialNativeInterface.getOSType());
         String[] portNames = SerialPortList.getPortNames(); 
+        System.out.println("Num name = " + portNames.length);
         for(String portName : portNames)
         {
             System.out.println(portName);
         }
+        SerialPort port = new SerialPort("COM0");
+        try
+        {
+            port.openPort();
+        }
+        catch(SerialPortException e)
+        {
+            System.err.println("Could not open port");
+        }
+        System.out.println("Done");
     }
 
 }
