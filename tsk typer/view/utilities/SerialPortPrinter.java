0f960144af19b983aecf7e5dd836d4184de5ea35
Mark Roukema <mroukema@gmail.com>
2014-06-12 12:50:27 -0400

    Changed port printer to try to open any port it finds

--- tsk typer/view/utilities/SerialPortPrinter.java
+++ tsk typer/view/utilities/SerialPortPrinter.java
@@ -3,25 +3,26 @@ import java.util.Enumeration;
 import jssc.SerialPort;
 import jssc.SerialPortException;
 import jssc.SerialPortList;
+import jssc.SerialNativeInterface;
 public class SerialPortPrinter 
 {
     public static void main(String[] args)
     {
-     //   System.out.println(SerialNativeInterface.getOSType());
+        //System.out.println(SerialNativeInterface.getOSType());
         String[] portNames = SerialPortList.getPortNames(); 
         System.out.println("Num name = " + portNames.length);
         for(String portName : portNames)
         {
             System.out.println(portName);
-        }
-        SerialPort port = new SerialPort("COM0");
-        try
-        {
-            port.openPort();
-        }
-        catch(SerialPortException e)
-        {
-            System.err.println("Could not open port");
+            SerialPort port = new SerialPort(portName);
+            try
+            {
+                port.openPort();
+            }
+            catch(SerialPortException e)
+            {
+                System.err.println("Could not open port");
+            }
         }
         System.out.println("Done");
     }
