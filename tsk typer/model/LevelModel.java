6ac7716ef1cfd4ffe04dd202bcabcae38051e541
saba <saba@saba-RF511-RF411-RF711>
2014-06-14 04:24:42 -0400

    Implemented level model and presenter

--- tsk typer/model/LevelModel.java
+++ tsk typer/model/LevelModel.java
@@ -9,25 +9,57 @@ public class LevelModel
     private String text_;   // full text
     private Map characterMap_;  // maps each character in text_ to a character mode
 
-    public enum CharacterMode {
-        CORRECT,
-        INCORRECT,
-        BAD_FORM
+    public enum CharacterMode
+    {
+        UNTYPED(0),
+        CORRECT(1),
+        INCORRECT(2),
+        BAD_FORM(3);
+
+        private int value_;
+
+        private CharacterMode(int value)
+        {
+            this.value_ = value;
+        }
+
+        public int getValue()
+        {
+            return value_;
+        }
     }
 
-    public LevelModel(String text) {
+    public LevelModel(String text)
+    {
         this.text_ = text;
         this.characterMap_ = new HashMap(text_.length());
     }
 
-    public CharacterMode getCharacterMode(int index) throws IllegalArgumentException {
-        if (index >= text_.length()) {
+    public int getTextLength()
+    {
+        return text_.length();
+    }
+
+    public CharacterMode getCharacterMode(int index) throws IllegalArgumentException
+    {
+        if (index >= text_.length())
+        {
             throw new IllegalArgumentException();
         }
         return (CharacterMode)this.characterMap_.get(index);
     }
 
-    public void setCharacterMode(int index, CharacterMode mode) {
+    public void setCharacterMode(int index, CharacterMode mode)
+    {
         this.characterMap_.put(index, mode);
+    }
+
+    public char getCharacter(int index) throws IllegalArgumentException
+    {
+        if (index >= text_.length())
+        {
+            throw new IllegalArgumentException();
+        }
+        return text_.charAt(index);
     }
 }
--- tsk typer/presenter/LevelPresenter.java
+++ tsk typer/presenter/LevelPresenter.java
@@ -1,12 +1,82 @@
 package presenter;
+
 import model.*;
+import java.lang.*;
 
 public class LevelPresenter
 {
-    LevelModel model_;
+    private LevelModel model_;
+    private int currentIndex_;  // index of current character
+    private int leftIndex_; // index of left-most character
+    private int rightIndex_;    // index of right-most character
+
+    private static final int windowSize_ = 50;
+
+    public enum CharacterColor
+    {
+        BLACK(0),
+        GREEN(1),
+        RED(2),
+        YELLOW(3);
+
+        private int value_;
+
+        private CharacterColor(int value)
+        {
+            this.value_ = value;
+        }
+
+        public int getValue()
+        {
+            return value_;
+        }
+    }
 
     public LevelPresenter()
     {
-        this.model_ = new LevelModel();
+    }
+
+    public void setTextContents(String text)
+    {
+        model_ = new LevelModel(text);
+        currentIndex_ = 0;
+        leftIndex_ = 0;
+        rightIndex_ = Math.min(model_.getTextLength(), leftIndex_ + windowSize_);
+    }
+
+    public int getLeftIndex()
+    {
+        return leftIndex_;
+    }
+
+    public int getRightIndex()
+    {
+        return rightIndex_;
+    }
+
+    public CharacterColor getCharacterColor(int index) throws IllegalArgumentException
+    {
+        try
+        {
+            LevelModel.CharacterMode mode = model_.getCharacterMode(index);
+            return (CharacterColor)mode.getValue();
+        }
+        catch (IllegalArgumentException e)
+        {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void handleTypedCharacter(char character)
+    {
+        if (model_.getCharacter(currentIndex_) == character)
+        {
+            model_.setCharacterMode(currentIndex_, LevelMode.CharacterMode.CORRECT);
+        }
+        else
+        {
+            model_.setCharacterMode(currentIndex_, LevelMode.CharacterMode.INCORRECT);
+        }
+        ++currentIndex_;
     }
 }
