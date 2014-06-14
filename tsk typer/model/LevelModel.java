dcecba27c739428274c6ae81e4a63dd8112ea1c9
saba <saba@saba-RF511-RF411-RF711>
2014-06-14 03:51:54 -0400

    Implemented LevelModel.java

--- tsk typer/model/LevelModel.java
+++ tsk typer/model/LevelModel.java
@@ -1,11 +1,33 @@
 package model;
 
-import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.lang.IllegalArgumentException;
 
 public class LevelModel
 {
+    private String text_;   // full text
+    private Map characterMap_;  // maps each character in text_ to a character mode
 
-    public LevelModel()
-    {
+    public enum CharacterMode {
+        CORRECT,
+        INCORRECT,
+        BAD_FORM
+    }
+
+    public LevelModel(String text) {
+        this.text_ = text;
+        this.characterMap_ = new HashMap(text_.length());
+    }
+
+    public CharacterMode getCharacterMode(int index) throws IllegalArgumentException {
+        if (index >= text_.length()) {
+            throw new IllegalArgumentException();
+        }
+        return (CharacterMode)this.characterMap_.get(index);
+    }
+
+    public void setCharacterMode(int index, CharacterMode mode) {
+        this.characterMap_.put(index, mode);
     }
 }
