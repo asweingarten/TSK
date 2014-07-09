7f14b0b9146f0206caa9ac24895ff4cdc47585f9
ARIEL WEINGARTEN <ASWEINGA@uwaterloo.ca>
2014-07-09 19:34:19 -0400

    Moved file-related code to Setup Screen model.

--- tsk typer/src/main/java/tsk_typer/TskTyperModel.java
+++ tsk typer/src/main/java/tsk_typer/TskTyperModel.java
@@ -1,7 +1,6 @@
 package tsk_typer;
 
 import interfaces.*;
-import utilities.*;
 import java.util.ArrayList;
 import java.io.File;
 import java.io.IOException;
@@ -10,8 +9,6 @@ import level.LevelModel;
 public class TskTyperModel extends BaseSubject
 {
     LevelModel levelModel_;
-    private TextFileReader reader_;
-    private String filename_;
     private Screen currentScreen_;
 
     public enum Screen
@@ -36,8 +33,6 @@ public class TskTyperModel extends BaseSubject
     public TskTyperModel()
     {
         this.observerList_ = new ArrayList<IObserver>();
-
-        reader_ = new TextFileReader(new FullFileReadStrategy());
     }
 
     public LevelModel getLevelModel()
@@ -65,39 +60,5 @@ public class TskTyperModel extends BaseSubject
     public Screen getCurrentScreen()
     {
         return currentScreen_;
-    }
-
-    public String getFilename()
-    {
-        return filename_;
-    }
-
-    public void setFilename(String filename)
-    {
-        filename_ = filename;
-    }
-
-    public String getFileContents()
-    {
-        try
-        {
-            return reader_.ReadFile(filename_);
-        }
-        catch (IOException ex)
-        {
-            return "";
-        }
-    }
-
-    public boolean isFileValid()
-    {
-        try
-        {
-            return new File(filename_).isFile();
-        }
-        catch (Exception ex)
-        {
-            return false;
-        }
     }
 }
