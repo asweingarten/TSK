b6608a507a912707d6960e2de2076fabf6ae4215
saba <saba@saba-RF511-RF411-RF711>
2014-08-07 20:19:40 -0400

    Added a basic test for formatted text in setup model

---
+++ tsk typer/src/test/java/unit/SetupModelTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+import setup_screen.*;
+import org.junit.Test;
+
+public class SetupModelTest
+{
+    private SetupModel getSetupModel()
+    {
+        return new SetupModel( System.getProperty( "user.dir" ) );
+    }
+
+    @Test
+    public void formattedTextTest()
+    {
+        String input = "hello\n\tworld    !?!   \n\n !  ";
+        String output = "hello world !?! !";
+        SetupModel model = getSetupModel();
+        model.setText( input );
+        assertEquals( output, model.getFormattedText() );
+    }
+}
