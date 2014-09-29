fb50631be016ac38d98aa3a459abf599f507011d
Ariel Weingarten <adawg@Ariels-MacBook-Pro-2.local>
2014-09-28 19:47:34 -0700

    Print center coords of interaction box.

--- leapmotion/InteractionBoxDemo.java
+++ leapmotion/InteractionBoxDemo.java
@@ -15,7 +15,7 @@ class InteractionBoxListener extends Listener
 		System.out.println("Connected");
 	}
 
-	public onDisconnect(Controller controller)
+	public void onDisconnect(Controller controller)
 	{
 		System.out.println("Disconnected");
 	}
@@ -25,9 +25,33 @@ class InteractionBoxListener extends Listener
 		System.out.println("Exited");
 	}
 
-	public void onFrames(Controller controller)
+	public void onFrame(Controller controller)
 	{
-		InteractionBox iBox = frame.getInteractionBox();
+		Frame frame = controller.frame();
+		InteractionBox iBox = frame.interactionBox();
 		System.out.println("Center: " + iBox.center());
+	}
+}
+
+class InteractionBoxDemo
+{
+	public static void main(String[] args)
+	{
+		InteractionBoxListener listener = new InteractionBoxListener();
+		Controller controller = new Controller();
+
+		controller.addListener(listener);
+
+		// Keep this process running until Enter is pressed
+        System.out.println("Press Enter to quit...");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Remove the sample listener when done
+        controller.removeListener(listener);
+
 	}
 }
